588712089be0a00d1301adbe85fad2a430154495
Stefan Countryman <stefan.countryman@gmail.com>
2020-07-06 20:05:02 -0400

    fix __version__, add __str__/__repr__/__doc__

--- lazytype.py
+++ lazytype.py
@@ -1,8 +1,9 @@
-"Read-only type wrappers for typing; module loading deferred till __init__."
+"Type wrappers for typing; module loading deferred till __init__."
 
-__version__ = 0.1.0
+__version__ = "0.1.0"
 
 import importlib
+from textwrap import indent
 from typing import Union
 
 
@@ -15,12 +16,45 @@ class LazyTypeMeta(type):
     def __instancecheck__(self, obj):
         return isinstance(obj, self._load_wraps())
 
-    @classmethod
     def __subclasscheck__(self, obj):
         return issubclass(obj, self._load_wraps())
 
 
 class LazyType(metaclass=LazyTypeMeta):
+    """
+    Get a wrapper class for the qualified name; module loading is deferred
+    until instantiation, which has the same interface as the wrapped object.
+    Attributes and subscripting refer to the wrapped class, except for the
+    special attributes ``_wraps`` (the wrapped class) and ``_instance`` (the
+    actual instance of the wrapped class). Note that setting attributes affects
+    the lazy wrapper, not the wrapped class; indexing passes directly through
+    to the wrapped instance, however.
+
+    Examples
+    --------
+    The interface is the same as the wrapped object:
+
+    >>> a = LazyType['numpy.ndarray']((3, 2))
+    >>> a
+    <Lazy array([[-1.49166815e-154, -2.68679856e+154],
+                 [ 1.48219694e-323,  0.00000000e+000],
+                 [ 0.00000000e+000,  4.17201348e-309]])>
+    >>> a._instance
+    array([[-2.00000000e+000,  2.32036240e+077],
+           [ 1.48219694e-323,  0.00000000e+000],
+           [ 0.00000000e+000,  4.17201348e-309]])
+    >>> a[:] = 0
+    >>> a
+    array([[0., 0.],
+           [0., 0.],
+           [0., 0.]])
+    >>> a.dtype
+    dtype('float64')
+    >>> isinstance(a._instance, type(a))
+    True
+
+    etc.
+    """
     _wraps: Union[str, type]
     _instance: str
 
@@ -33,6 +67,12 @@ class LazyType(metaclass=LazyTypeMeta):
 
     def __init__(self, *args, **kwargs):
         self._instance = self._load_wraps()(*args, **kwargs)
+
+    def __str__(self):
+        return '<Lazy %s>'%indent(str(self._instance), ' '*6)[6:]
+
+    def __repr__(self):
+        return '<Lazy %s>'%indent(repr(self._instance), ' '*6)[6:]
 
     def __getitem__(self, key):
         return self._instance[key]
